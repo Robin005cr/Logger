9b7c29696a904aa39eb9a3219eb2e45d8c946706
Robin CR <robin005cr@gmail.com>
2025-07-17 22:47:59 +0530

    Added a base level structure for the project

---
+++ Logger_/src/WarningHandler.cpp
@@ -0,0 +1,12 @@
+/*
+ * project   : https://github.com/Robin005cr/Logger
+ * file name : WarningHandler.hpp
+ * author    : Robin CR
+ * mail id   : robinchovallurraju@gmail.com
+ * portfolio : https://robin005cr.github.io/
+ *
+ * Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+ * Suggestions for improvements or better methods are always welcome and appreciated.
+ * I value constructive feedback and aim to continuously improve the quality of the work.
+ *
+ */
